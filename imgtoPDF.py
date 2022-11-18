4e26ca31e763b6e14f48d599ceb30adfa66899e7
Shehan Pramitha <pwshehan@gmail.com>
2022-11-18 22:16:36 +0530

    Changes

--- imgtoPDF.py
+++ imgtoPDF.py
@@ -29,6 +29,8 @@ for file in files:
         "newPage": newPage
     })
 
+bar = ChargingBar('Creating PDF', max=len(images))
+
 pages = []
 
 pages.append([images.pop(0)])
@@ -56,46 +58,38 @@ prac_num = input("Enter Practical Number: ")
 prac_name = input("Enter Practical Name: ")
 sub_date = input("Enter Submission Date: ")
 
-ln1 = "Module Code: ER2034"
-ln2 = "Module Name: Principles of Remote Sensing and GIS"
-ln3 = f"Practical {prac_num} : {prac_name}"
-ln4 = f"Name: {name}"
-ln5 = f"Index No: {index_no}"
-ln6 = f"Submission Date: {sub_date}"
-
-bar = ChargingBar('Creating PDF', max=len(images))
-
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial", size=15)
 pdf.set_text_color(0, 0, 0)
-pdf.text(10, 20, txt=ln1)
-pdf.text(10, 30, txt=ln2)
-pdf.text(10, 40, txt=ln3)
-pdf.text(10, 50, txt=ln4)
-pdf.text(10, 60, txt=ln5)
-pdf.text(10, 70, txt=ln6)
+pdf.text(15, 20, txt="Module Code: ER2034")
+pdf.text(15, 30, txt="Module Name: Principles of Remote Sensing and GIS")
+pdf.text(15, 40, txt=f"Practical No: {prac_num}")
+pdf.text(15, 50, txt=f"Practical Name: {prac_name}")
+pdf.text(15, 60, txt=f"Name: {name}")
+pdf.text(15, 70, txt=f"Index No: {index_no}")
+pdf.text(15, 80, txt=f"Submission Date: {sub_date}")
 
 pdf.set_font("Arial", size=10)
 image1 = pages.pop(0)[0]
-pdf.text(10, 90, txt=image1["title"])
-pdf.image(image1["filepath"], x=10, y=95, w=180)
+pdf.text(15, 100, txt=image1["title"])
+pdf.image(image1["filepath"], x=15, y=105, w=180)
 bar.next()
 
 for page in pages:
     pdf.add_page()
 
     image = page.pop(0)
-    pdf.text(10, 20, txt=image["title"])
-    pdf.image(image["filepath"], x=10, y=25, w=180)
+    pdf.text(15, 20, txt=image["title"])
+    pdf.image(image["filepath"], x=15, y=25, w=180)
     bar.next()
 
     if not len(page):
         continue
 
     image = page.pop(0)
-    pdf.text(10, 150, txt=image["title"])
-    pdf.image(image["filepath"], x=10, y=155, w=180)
+    pdf.text(15, 150, txt=image["title"])
+    pdf.image(image["filepath"], x=15, y=155, w=180)
     bar.next()
 
 bar.finish()
